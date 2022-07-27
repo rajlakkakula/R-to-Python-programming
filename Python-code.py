f9cd31e684d69151310ab436830a93fc8d9c02c2
rajlakkakula <prithvilakkakula@gmail.com>
2022-07-27 00:26:26 -0500

    basic programming and data types

--- Python-code.py
+++ Python-code.py
@@ -3,4 +3,30 @@ import pandas as pd
 df = pd.DataFrame({'a': [1, 2, 3, 4, 5],
                           'b': [3, 4, 5, 6, 7]})
 df
-#set(df['a']) & set(df['b'])
+
+## imports
+import math
+math.sqrt(36)
+
+#importing a function
+from math import sqrt
+sqrt(36)
+
+# show all functions in math model
+print(dir(math))
+
+## Data types
+type(5)
+type(5.0)
+type('five')
+type(False)
+
+## check if an object is of a given type
+isinstance(5.0, int)
+isinstance(5.0, (int, float))
+isinstance('Five', str)
+isinstance('Five', int)
+isinstance(False, bool)
+
+## Convert an object to a given type
+str(5.5)
--- R-code.R
+++ R-code.R
@@ -1,5 +1,22 @@
-# construct a dataframe
+## construct a dataframe
 df <- data.frame(a = c(1, 2, 3, 4, 5),
                  b = c(3, 4, 5, 6, 7))
 df
-#intersect(df$a, df$b)
+
+## imports
+sqrt(36)
+
+## Data types
+class(5)
+class(5.0)
+class('Five')
+class(FALSE)
+
+## check if an object is of a given type
+is.numeric(5)
+is.character('Five')
+is.logical(FALSE)
+
+## Convert an object to a given type
+as.character(5.5)
+#as.numeric, as.factor etc to convert to numeric and factor types
